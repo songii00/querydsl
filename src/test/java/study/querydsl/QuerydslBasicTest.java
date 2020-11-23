a0f2d62387f0f46ddaddc4633d90b2cebfebcccb
zoe <wks4j1004@gmail.com>
2020-11-24 01:17:05 +0900

    검색조건 & 결과조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,8 @@ import javax.swing.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.member;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -77,11 +80,75 @@ public class QuerydslBasicTest {
     @Test
     public void startQuerydsl2(){
 
-        Member findMember = queryFactory.select(member) // 큐타입 스태틱 임포트로 활용 
+        Member findMember = queryFactory.select(member) // 큐타입 스태틱 임포트로 활용
                                         .from(member)
                                         .where(member.username.eq("member1"))
                                         .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    public void search(){
+        Member findMember
+                = queryFactory.selectFrom(member)
+                                     .where(member.username.eq("member1")
+                                                           .and(member.age.eq(10)))
+                                     .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void searchAndParam(){
+        Member findMember
+                = queryFactory.selectFrom(member)
+                              .where(
+                                      member.username.eq("member1"),
+                                      member.age.eq(10)
+                              )
+                              .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+
+    @Test
+    public void resultFetch(){
+
+        /**
+         * 주의 > 컨텐츠 가져오는 쿼리가 복잡하면 성능을 위해서 토탈카운트 쿼리와 컨텐츠 가져오는 쿼리가 다를수 있음
+         * 복잡하고 성능 중요한 쿼리에선 사용하면 안됨
+         */
+        QueryResults<Member> results = queryFactory.selectFrom(member)
+                                                              .fetchResults();
+
+        // 카운트 쿼리
+        // select
+        //        count(member0_.member_id) as col_0_0_
+        //    from
+        //        member member0_
+        results.getTotal();
+        // 컨텐츠 쿼리
+        //  select
+        //        member0_.member_id as member_i1_1_,
+        //        member0_.age as age2_1_,
+        //        member0_.team_id as team_id4_1_,
+        //        member0_.username as username3_1_
+        //    from
+        //        member member0_
+        List<Member> contents = results.getResults();
+
+        // 카운트 절로 바꿈
+        // select
+        //        count(member0_.member_id) as col_0_0_
+        //    from
+        //        member member0_
+        long count = queryFactory.selectFrom(member)
+                             .fetchCount();
+    }
+
+
 }
